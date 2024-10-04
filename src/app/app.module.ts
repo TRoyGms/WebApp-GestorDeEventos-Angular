053ad6f0c3a7667b0f704643c786b8d8bd9ee6e2
TRoyGms <roy89_gona@hotmail.com>
2024-10-03 22:40:02 -0600

    Components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventoComponent } from './components/evento/evento.component';
+import { ParticipanteComponent } from './components/participante/participante.component';
+import { RegistroComponent } from './components/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventoComponent,
+    ParticipanteComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/participante/participante.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-participante',
+  templateUrl: './participante.component.html',
+  styleUrl: './participante.component.css'
+})
+export class ParticipanteComponent {
+
+}
